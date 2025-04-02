a6cf9c5be88adcf8c8c0f4409514c19acb32b37c
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2025-04-02 21:48:49 +0530

    fix: update email config

--- config/emailConfig.ts
+++ config/emailConfig.ts
@@ -1,10 +1,10 @@
 import nodemailer from 'nodemailer'
 
 let transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    service: 'gmail',
-    secure: false, // true for 465, false for other ports
+    host: process.env.EMAIL_HOST,
+    port: 465,
+    // service: 'gmail',
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER, // Admin Gmail ID
         pass: process.env.EMAIL_PASS, // Admin Gmail Password
