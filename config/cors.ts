bd4403bef910b6f4e9638867f35885a0ab31324d
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2025-04-24 15:06:40 +0530

    fix: update cors origin

--- config/cors.ts
+++ config/cors.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import { RequestHandler } from 'express';
 
 const corsMiddleware: RequestHandler = cors({
-    origin: "*",
+    origin: "https://sbiea.co.in, https://www.sbiea.co.in, https://admin.sbiea.co.in, https://institution.sbiea.co.in, https://student.sbiea.co.in", // Allow requests from these origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  // Methods should be an array
     allowedHeaders: ["Content-Type", "Authorization"],      // Add commonly needed headers
     credentials: true,
