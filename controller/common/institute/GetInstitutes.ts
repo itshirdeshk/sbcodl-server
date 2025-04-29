d00e5fc968bda103db4237e3296577315cc38d31
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2025-04-29 10:51:53 +0530

    fix: update getInstitutes Controller

--- controller/common/institute/GetInstitutes.ts
+++ controller/common/institute/GetInstitutes.ts
@@ -5,14 +5,11 @@ import { GetInstitutesRequestSchema } from "../../../validation/common/institute
 export const GetInstitutes = async (req: ValidatedRequest<GetInstitutesRequestSchema>) => {
     const { applicationNumber, centerCode, registrationNumber, headAadharNumber, headEmailId, headMobileNumber, headPanCardNumber, limit, skip } = req.body;
 
-    const institute = await prisma.institute.findMany({
+    const institutes = await prisma.institute.findMany({
         where: { applicationNumber, centerCode, registrationNumber, headAadharNumber, headEmailId, headMobileNumber, headPanCardNumber },
-        include: {
-            documents: true,
-        },
         skip: skip,
         take: limit
 
     });
-    return institute;
+    return institutes;
 }
